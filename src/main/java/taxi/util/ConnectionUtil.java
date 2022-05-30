d88b46fcc070a345e9a269ddce10bf44e0c6f931
Vova Klimyuk <klimyuk054@gmail.com>
2022-05-30 17:36:16 +0300

    fixed url DB

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String URL = "jdbc:mysql://"
-            + "us-cdbr-east-05.cleardb.net:3306/heroku_8411acf8e71aecc?reconnect=true";
+            + "us-cdbr-east-05.cleardb.net:3306/heroku_8411acf8e71aecc";
     private static final String USERNAME = "b50fadacb2bba3";
     private static final String PASSWORD = "b945d85f";
 
